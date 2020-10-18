6146f764b6457a5b77b785645f063d8ef434c6ac
Masaki Hara <ackie.h.gmai@gmail.com>
2020-10-18 14:29:24 +0900

    Use SCRIPT_LOG_FILE

--- script.js
+++ script.js
@@ -11,5 +11,13 @@ if (hasDevDependencies) {
   msg += " (with dev deps)";
 }
 
-console.log(msg);
-fs.promises.writeFile("script.log", msg + "\n", { flag: "a" });
+const logFile = process.env.SCRIPT_LOG_FILE;
+if (!logFile) {
+  console.error("SCRIPT_LOG_FILE not set");
+  process.exit(1);
+}
+if (!logFile.startsWith("/")) {
+  console.error(`SCRIPT_LOG_FILE must be an absolute path; got ${logFile}`);
+  process.exit(1);
+}
+fs.promises.writeFile(logFile, msg + "\n", { flag: "a" });
