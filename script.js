2d5772281f278d243a9c9a31142b784ed443c071
Masaki Hara <ackie.h.gmai@gmail.com>
2020-10-24 16:00:59 +0900

    Use var for old Node.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
+var fs = require("fs");
 
-let hasDevDependencies = false;
+var hasDevDependencies = false;
 try {
   require("is-value");
   hasDevDependencies = true;
 } catch (e) { }
 
-let msg = `${process.env.npm_package_name}: ${process.env.npm_lifecycle_event} at ${__dirname}`;
+var msg = `${process.env.npm_package_name}: ${process.env.npm_lifecycle_event} at ${__dirname}`;
 if (hasDevDependencies) {
   msg += " (with dev deps)";
 }
 
-const logFile = process.env.SCRIPT_LOG_FILE;
+var logFile = process.env.SCRIPT_LOG_FILE;
 if (!logFile) {
   console.error("SCRIPT_LOG_FILE not set");
   process.exit(1);
